ffedd87a4ba4c28b034a6db117c589ef5bc86d21
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-13 22:09:30 +0600

    user based private route based function included

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,37 @@ import type { NextRequest } from "next/server";
 
 import { NextResponse } from "next/server";
 
+const AuthRoutes = ["/register", "/login"];
+const roleBasedRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  // const user = {
-  //   name: "Rahman",
-  //   token: "sigjwprg",
-  //   role: "USER",
-  // };
-  let user = undefined
+  const { pathname } = request.nextUrl;
+  console.log(pathname);
+  const user = {
+    name: "Rahman",
+    token: "sigjwprg",
+    role: "USER",
+  };
+  // let user = undefined;
 
-  if (user?.name) {
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  if(user?.role && roleBasedRoutes[user?.role]) {
+   const routes = roleBasedRoutes[user?.role]
+
+   if(routes.some((route) => pathname.match(route))){
     return NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/login", request.url));
+   }
   }
 
   return NextResponse.redirect(new URL("/", request.url));
@@ -22,5 +40,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/admin"],
+  matcher: ["/profile", "/admin", "/login", "/register"],
 };
