ecf4fd252485bd42b42777eec47f86efab036592
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-19 22:31:36 +0600

    item post creation done

--- src/services/RecentPosts/index.ts
+++ src/services/RecentPosts/index.ts
@@ -1,8 +1,13 @@
 import envConfig from "@/src/config/envConfig";
 
 export const getRecentPosts = async () => {
+  const fetchOptions = {
+    next: {
+      tags: ['posts']
+    }
+  }
   const res = await fetch(
-    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`
+    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`, fetchOptions
   );
 
   return res.json();
