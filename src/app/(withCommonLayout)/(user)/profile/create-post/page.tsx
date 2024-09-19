59a3e2d6a8614d3339a295a0ea6ec6b355c2e5d1
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-19 21:03:51 +0600

    text area and select options added

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -13,6 +13,9 @@ import {
 import FXInput from "@/src/components/form/FXInput";
 import { Divider } from "@nextui-org/divider";
 import dateToISO from "@/src/utils/dateToISO";
+import FXDatePicker from "@/src/components/form/FXDatePicker";
+import FXSelect from "@/src/components/form/FXSelect";
+import FXTextarea from "@/src/components/form/FXTextArea";
 
 const CreatePost = () => {
   const methods = useForm();
@@ -37,30 +40,113 @@ const CreatePost = () => {
   };
 
   return (
-    <div>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FXInput label="Title" name="title" />
-
-          <Divider className="my-5" />
-
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl">Owner verification questions</h1>
-            <Button onClick={() => handleFieldAppend()}>Append</Button>
-          </div>
-
-          {fields.map((field, index) => (
-            <div key={field.id} className="flex items-center">
-              <FXInput name={`questions.${index}.value`} label="Question" />
-              <Button onClick={() => remove(index)}>Remove</Button>
+    <>
+      {createPostPending && <Loading />}
+      <div className="h-full rounded-xl bg-gradient-to-b from-default-100 px-[73px] py-12">
+        <h1 className="text-2xl font-semibold">Post a found item</h1>
+        <Divider className="mb-5 mt-3" />
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXInput label="Title" name="title" />
+              </div>
+              <div className="min-w-fit flex-1">
+                <FXDatePicker label="Found date" name="dateFound" />
+              </div>
             </div>
-          ))}
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXInput label="Location" name="location" />
+              </div>
+              <div className="min-w-fit flex-1">
+                <FXSelect label="City" name="city" options={cityOptions} />
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXSelect
+                  disabled={!categorySuccess}
+                  label="Category"
+                  name="category"
+                  options={categoryOption}
+                />
+              </div>
+              <div className="min-w-fit flex-1">
+                <label
+                  className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
+                  htmlFor="image"
+                >
+                  Upload image
+                </label>
+                <input
+                  multiple
+                  className="hidden"
+                  id="image"
+                  type="file"
+                  onChange={(e) => handleImageChange(e)}
+                />
+              </div>
+            </div>
 
-          <Button type="submit">Post</Button>
-        </form>
-      </FormProvider>
-    </div>
+            {imagePreviews.length > 0 && (
+              <div className="flex gap-5 my-5 flex-wrap">
+                {imagePreviews.map((imageDataUrl) => (
+                  <div
+                    key={imageDataUrl}
+                    className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
+                  >
+                    <img
+                      alt="item"
+                      className="h-full w-full object-cover object-center rounded-md"
+                      src={imageDataUrl}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+
+            <div className="flex flex-wrap-reverse gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXTextarea label="Description" name="description" />
+              </div>
+            </div>
+
+            <Divider className="my-5" />
+
+            <div className="flex justify-between items-center mb-5">
+              <h1 className="text-xl">Owner verification questions</h1>
+              <Button isIconOnly onClick={() => handleFieldAppend()}>
+                <AddIcon />
+              </Button>
+            </div>
+
+            <div className="space-y-5">
+              {fields.map((field, index) => (
+                <div key={field.id} className="flex gap-2 items-center">
+                  <FXInput label="Question" name={`questions.${index}.value`} />
+                  <Button
+                    isIconOnly
+                    className="h-14 w-16"
+                    onClick={() => remove(index)}
+                  >
+                    <TrashIcon />
+                  </Button>
+                </div>
+              ))}
+            </div>
+
+            <Divider className="my-5" />
+            <div className="flex justify-end">
+              <Button size="lg" type="submit">
+                Post
+              </Button>
+            </div>
+          </form>
+        </FormProvider>
+      </div>
+    </>
   );
-};
+}
 
 export default CreatePost;
