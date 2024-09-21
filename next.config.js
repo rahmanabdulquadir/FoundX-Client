49d864b21e7fc14e3fc0c60aa5d4e531e48d5c1f
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-21 22:07:22 +0600

    nextjs file configured

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
--- src/components/UI/Post/ImageGallery.tsx
+++ src/components/UI/Post/ImageGallery.tsx
@@ -1,13 +1,27 @@
+"use client"
+
 import LightGallery from "lightgallery/react";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
+import Link from "next/link";
+import Image from "next/image";
 
-const ImageGallery = () => {
+type IProps = {
+  images: string[];
+};
+
+const ImageGallery = ({ images }: IProps) => {
   return (
     <LightGallery plugins={[lgThumbnail, lgZoom]} speed={500}>
+      {images?.map((image, index) => (
+        <Link key={index} href={image}>
+          <Image src={image} height={500} width={500} alt={`image- ${index}`} />
+        </Link>
+      ))}
+
       <a href="img/img1.jpg">
         <img alt="img1" src="img/thumb1.jpg" />
       </a>
