4edccba65619d735e5eb8e9159f995889732e859
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-22 18:32:03 +0600

    claim request related service added

--- src/hooks/claimRequest.hook.ts
+++ src/hooks/claimRequest.hook.ts
@@ -2,6 +2,8 @@ import { useMutation } from "@tanstack/react-query";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 
+import { addClaimRequest } from "../services/ClaimRequest";
+
 export const useAddClaimRequest = () => {
   return useMutation<any, Error, FieldValues>({
     mutationKey: ["ADD_CLAIM_REQUEST"],
---
+++ src/services/ClaimRequest/index.ts
@@ -0,0 +1,16 @@
+import { FieldValues } from "react-hook-form";
+
+import axiosInstance from "@/src/lib/AxiosInstance";
+
+
+export const addClaimRequest = async (
+  claimRequest: FieldValues
+): Promise<any> => {
+  try {
+    const res = await axiosInstance.post("/claim-request", claimRequest);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
