d7b174d52b677847a618b99ec0eddee7c78d97cc
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-22 18:15:54 +0600

    found items page modified

--- src/app/(withCommonLayout)/found-items/page.tsx
+++ src/app/(withCommonLayout)/found-items/page.tsx
@@ -1,10 +1,17 @@
+import Container from '@/src/components/UI/Container';
+import axiosInstance from '@/src/lib/AxiosInstance';
+import { IPost } from '@/src/types';
 import React from 'react';
 
-const page = () => {
-  return (
-    <div>
-      This is found item page
+const page = async() => {
+  const { data } = await axiosInstance.get(`/items`);
+
+  return ( 
+    <Container>
+    <div className="mx-auto my-3 max-w-[720px]">
+      {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
     </div>
+  </Container>
   );
 };
 
