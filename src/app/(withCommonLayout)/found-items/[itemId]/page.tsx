769a30d6a685c0fbe0ab38be05314755a76bec56
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-22 22:32:49 +0600

    dynamic individual post page created

---
+++ src/app/(withCommonLayout)/found-items/[itemId]/page.tsx
@@ -0,0 +1,22 @@
+import Container from "@/src/components/UI/Container";
+import Post from "@/src/components/UI/Post";
+
+interface IProps {
+  params: {
+    itemId: string;
+  };
+}
+
+const ItemDetailPage = async ({ params: { itemId } }: IProps) => {
+  const { data: post } = await getPost(itemId);
+
+  return (
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        <Post key={post?._id} post={post} />
+      </div>
+    </Container>
+  );
+};
+
+export default ItemDetailPage;
