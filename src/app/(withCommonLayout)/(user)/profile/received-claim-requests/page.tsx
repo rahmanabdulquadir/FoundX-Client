2d0200edab8de5ee75711879ad1ac533258082be
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-24 21:06:16 +0600

    received claim request comp added

---
+++ src/app/(withCommonLayout)/(user)/profile/received-claim-requests/page.tsx
@@ -0,0 +1,21 @@
+const ReceivedClaimRequestsPage = () => {
+  const { data } = useGetReceivedClaimRequest();
+
+  const posts = data?.data || [];
+
+  return (
+    <>
+      {posts?.length ? (
+        posts.map((post: IReceivedClaimRequest, index: number) => (
+          <ClaimPostCard key={index} post={post} />
+        ))
+      ) : (
+        <div className="flex min-h-screen w-full items-center justify-center rounded-md bg-default-100">
+          <h1 className="text-4xl">No Claim Request Received!</h1>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ReceivedClaimRequestsPage;
