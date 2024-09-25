863fcc33aa629b02b82224d58c83b401d872ca7a
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-25 17:00:32 +0600

    access token retrieved by refresh token

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -5,9 +5,9 @@ import { cookies } from "next/headers";
 import axiosInstance from "@/src/lib/AxiosInstance";
 import { jwtDecode } from "jwt-decode";
 
-export const registerUser = async (userInfo: FieldValues) => {
+export const registerUser = async (userData: FieldValues) => {
   try {
-    const { data } = await axiosInstance.post("/auth/register", userInfo);
+    const { data } = await axiosInstance.post("/auth/register", userData);
 
     if (data.success) {
       cookies().set("accessToken", data?.data?.accessToken);
@@ -15,12 +15,10 @@ export const registerUser = async (userInfo: FieldValues) => {
     }
 
     return data;
-    // console.log(res.data);
   } catch (error: any) {
     throw new Error(error);
   }
 };
-
 
 export const loginUser = async (userData: FieldValues) => {
   try {
@@ -37,22 +35,20 @@ export const loginUser = async (userData: FieldValues) => {
   }
 };
 
-
 export const logout = () => {
   cookies().delete("accessToken");
   cookies().delete("refreshToken");
-}
-
+};
 
 export const getCurrentUser = async () => {
   const accessToken = cookies().get("accessToken")?.value;
 
-  let decodedToken = null
+  let decodedToken = null;
 
-  if(accessToken){
-    decodedToken = await jwtDecode(accessToken)
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
 
-    return{
+    return {
       _id: decodedToken._id,
       name: decodedToken.name,
       email: decodedToken.email,
@@ -60,8 +56,27 @@ export const getCurrentUser = async () => {
       role: decodedToken.role,
       status: decodedToken.status,
       profilePhoto: decodedToken.profilePhoto,
-    }
+    };
   }
 
-  return decodedToken
-}
+  return decodedToken;
+};
+
+export const getNewAccessToken = async () => {
+  try {
+    const refreshToken = cookies().get("refreshToken")?.value;
+
+    const res = await axiosInstance({
+      url: "/auth/refresh-token",
+      method: "POST",
+      withCredentials: true,
+      headers: {
+        cookies: `refreshToken=${refreshToken}`,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to get new access token");
+  }
+};
